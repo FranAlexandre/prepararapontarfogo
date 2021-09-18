fe8f8a176656340e79abe593f3c619ca9291e4ee
franciny arielly <francinyalexandre07@gmail.com>
2021-09-18 14:40:49 -0300

    resolução exercicio prepararapontarfogo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace prepararapontarfogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("preparar");
+            Console.ReadKey();
+            Console.WriteLine("apontar");
+            Console.ReadKey();
+            Console.WriteLine("fogo");
+        }
+    }
+}
